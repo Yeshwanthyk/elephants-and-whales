6ad5a4ee4b61fcf7f0f8310f7013cf6c7d140a08
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-10-29 00:53:47 -0400

    Grab and show cloudinary images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import styled from 'styled-components';
-
-import { Box, Button, Flex } from '@chakra-ui/core';
+import axios from 'axios';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
+import { Box, Button, Flex, Image as ChakraImage } from '@chakra-ui/core';
 
 const Input = styled.input`
   width: 0.1px;
@@ -26,6 +27,7 @@ const InputLabel = styled.label`
 
 export default function Home() {
   const [primaryImage, setPrimaryImage] = useState('');
+  const [randImage, setRandImage] = useState('');
 
   const { register, handleSubmit, watch, errors } = useForm();
 
@@ -62,17 +64,38 @@ export default function Home() {
         <link rel="icon" href="../src/assets/favicon.ico" />
       </Head>
 
-      <Flex flexDirection="column" m={4}>
-        <Box py={4}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Input type="file" name="file" id="file" ref={register} />
-            <InputLabel htmlFor="file">Choose a file</InputLabel>
-            {errors.selectFile && <span>This field is required</span>}
-            <input type="submit" />
-          </form>
-        </Box>
-        <Box>{<img src={primaryImage} />}</Box>
-      </Flex>
+      <Box>
+        <Flex flexDirection="column" m={4}>
+          <Box py={4}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input type="file" name="file" id="file" ref={register} />
+              <InputLabel htmlFor="file">Choose a file</InputLabel>
+              {errors.selectFile && <span>This field is required</span>}
+              <input type="submit" />
+            </form>
+            <Button my={4} onClick={shuffleImages}>
+              Rand
+            </Button>
+          </Box>
+
+          <Flex align="center" justify="center" m={4}>
+            <Box px={4}>
+              <ChakraImage src={primaryImage} />
+            </Box>
+            <Box>
+              <CloudinaryContext cloudName="yesh">
+                <Image publicId={randImage}>
+                  <Transformation
+                    crop="scale"
+                    dpr="auto"
+                    responsive_placeholder="blank"
+                  />
+                </Image>
+              </CloudinaryContext>
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
     </Box>
   );
 }
